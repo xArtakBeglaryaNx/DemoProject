624a4f7292bc04fd3038d2c1ccc86247ca24c704
ARTAK BEGLARYAN <beglaryan.art.ed@gmail.com>
2022-11-16 23:39:34 +0600

    Little correction view

--- DemoProjectAspNetMVC/Program.cs
+++ DemoProjectAspNetMVC/Program.cs
@@ -31,7 +31,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
 ));
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+// builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
